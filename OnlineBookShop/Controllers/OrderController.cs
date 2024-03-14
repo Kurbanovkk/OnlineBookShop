ff6625f6f13102f897100bae90e5475be0add627
Курбан Курбанов <kurbanovkk96@mail.ru>
2024-03-14 17:17:11 +0300

    forms

---
+++ OnlineBookShop/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineBookShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ICartsRepository _cartsRepository;
+
+        public OrderController(ICartsRepository cartsRepository)
+        {
+            _cartsRepository = cartsRepository;
+        }
+        public IActionResult Index()
+        {
+            var cart = _cartsRepository.TryGetByUserId(Constants.UserId);
+            return View(cart);
+        }
+
+        public IActionResult Order()
+        {
+            _cartsRepository.Clear(Constants.UserId);
+            return View("/Views/Order/OrderEnd.cshtml");
+        }
+    }
+}
