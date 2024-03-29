bb68b532cd35afcfa3ecdff24863e5618130e77a
Курбан Курбанов <kurbanovkk96@mail.ru>
2024-03-29 16:33:25 +0300

    search

--- OnlineBookShop/Controllers/HomeController.cs
+++ OnlineBookShop/Controllers/HomeController.cs
@@ -28,5 +28,17 @@ namespace OnlineBookShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult Search(string name)
+        {
+            if (name != null)
+            {
+                var products = _productRepository.GetProducts();
+                var findProduct = products.Where(product => product.Name.ToLower().Equals(name.ToLower())).ToList();
+                return View(findProduct);
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
