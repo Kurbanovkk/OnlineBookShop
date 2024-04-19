fd7a7713f557a4ebfe2b8e3ef9ab28a070e25a2b
Курбан Курбанов <kurbanovkk96@mail.ru>
2024-04-19 17:26:21 +0300

    12

--- OnlineBookShop/Models/Login.cs
+++ OnlineBookShop/Models/Login.cs
@@ -1,10 +1,14 @@
-﻿namespace OnlineBookShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBookShop.Models
 {
     public class Login
     {
+        [Required(ErrorMessage = "Не указано имя пользователя")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
         public bool rememberMe { get; set; }
 
     }
